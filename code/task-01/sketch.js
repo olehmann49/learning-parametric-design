43f0360a01af117d4f5c92cbeff678f5efbcfffc
Oliver Lehmann <olehmann49@gmail.com>
2022-04-07 16:54:57 +0200

    Update sketch.js

--- code/task-01/sketch.js
+++ code/task-01/sketch.js
@@ -3,10 +3,86 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(400, 600
+  createCanvas(800, 1600, 
     );
 }
 
 function draw() {
-  background(100);
+  // Großer Kreis Im Hintergrund
+  background(20);
+
+  fill('red')
+  circle(400, 400, 600);
+  // 2x 90° Winkel in Schwarz
+  angleMode(DEGREES);
+
+  fill('black');
+  arc( 
+    400, 400,
+    600, 600,
+    90,
+    180,
+    PIE
+  );
+
+  fill('black');
+  arc( 
+    400, 400,
+    600, 600,
+    270,
+    0,
+    PIE
+  );
+
+  // 4x kleiner Halbkreis darüber
+  arc( 
+    550, 400,
+    300, 300,
+    0,
+    180,
+    PIE
+  );
+
+  fill('red');
+  arc( 
+    550, 400,
+    300, 300,
+    180,
+    0,
+    PIE
+  );
+  fill('red')
+   arc( 
+    250, 400,
+    300, 300,
+    0,
+    180,
+    PIE
+  );
+  
+  fill('black');
+  arc( 
+    250, 400,
+    300, 300,
+    180,
+    0,
+    PIE
+  );
+  // ------------------------------------------------------------------
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
